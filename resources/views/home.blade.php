a5aedec29a8f1009b716e75fd8688c6e5780284a
Agcel，Estive <estive.agcel.z@dc.kyocera.com>
2019-11-25 10:59:01 +0800

    search

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,13 +21,13 @@
         <div class="container col-md-6">
             <div class="row">
                 <div class="active-cyan-4 mb-4 col-sm-10">
-                    <input class="form-control" type="text" placeholder="Enter keywords" aria-label="Search">
+                    <input class="form-control" type="text" placeholder="Enter keywords" aria-label="Search" id="search" name="search">
                 </div>
                 <div class="container col-sm-2 px-0">
                     <button type="button btn-block" class="btn btn-outline-info">Search</button>
                 </div>
             </div>
-            <table class="table table-hover table-sm">
+            <table class="table table-hover table-sm" id="table">
                 <thead>
                     <tr>
                         <th scope="col">No</th>
@@ -124,5 +124,38 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+        $("#table").hide();
+        $("#loading").hide();
+        $('#search').on('keyup',function(){
+        $value=$(this).val();
+        $.ajax({
+            type : 'get',
+            url : '{{URL::to('search')}}',
+            data:{'search':$value},
+            beforeSend: function(){
+                $("#loading").show();
+                $("#table").hide();
+            },
+            complete: function(){
+                $("#loading").hide();
+                $("#table").show();
+            },
+            success:function(data){
+                $('tbody').html(data);
+            }
+        });
+        })
+
+        $(function() {
+            $('table.cont').on("click", "tr.table-tr", function() {
+                var $item = $(this).text();         // Retrieves the text within <td>
+                alert($item);
+            });
+        });
+        </script>
+        <script type="text/javascript">
+            $.ajaxSetup({ headers: { 'csrftoken' : '{{ csrf_token() }}' } });
+        </script>
   </body>
 </html>
